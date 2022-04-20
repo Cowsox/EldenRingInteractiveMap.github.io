deea4ed86c3cee5faaa9cca2f4cdf78542872064
Cowsox <87813162+Cowsox@users.noreply.github.com>
2022-04-20 17:23:37 +1000

    Auto Update

--- js/ERMap.js
+++ js/ERMap.js
@@ -1,3 +1,21 @@
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      // Swap it in and reload the page to get the new hotness.
+      window.applicationCache.swapCache();
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
+
 let mapBounds = 2000;
 let myBounds = [[0, 0],[mapBounds, mapBounds]];
 
